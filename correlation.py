490ae351a1271920d902bc1f26130fecc198d06a
kosurusravani <49892033+kosurusravani@users.noreply.github.com>
2019-04-23 11:41:42 +0530

    Add files via upload

---
+++ correlation.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+def reverse(h):
+	n1=len(h)
+	y=[]
+	for i in range(0,n1,1):
+		s=h[n1-1-i]
+		y=np.append(y,s)
+	return y
+def convolute(x,h):
+	n1=len(x)
+	n2=len(h)
+	y=[]
+	for n in range(n1+n2-1):
+		sum=0
+		for k in range(n1):
+			if n-k>=0and n-k<=n2-1:
+				sum=sum+x[k]*h[n-k]
+		y=np.append(y,sum)
+	return y
+x=np.array(input('enter seq1:'))
+h=np.array(input('enter seq2:'))
+print(convolute(x,reverse(h)))
+print('correlation=',np.convolve(x,reverse(h)))
